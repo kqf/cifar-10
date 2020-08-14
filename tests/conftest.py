ec3cb41581ff93c6b9164e6c5fc4beb1e9ca69d1
Sasha <q.dev.null@gmail.com>
2020-08-14 18:29:53 +0200

    Fix the tests for the CNN features

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,18 @@
 import pytest
+import torch
 import torchvision
 
-PRETRAINED_SIZE = 224
-PRETRAINED_MEANS = [0.485, 0.456, 0.406]
-PRETRAINED_STDS = [0.229, 0.224, 0.225]
-
 
 @pytest.fixture
-def train():
-    # See https://pytorch.org/docs/stable/torchvision/models.html
+def data():
+    pretrained_size = 224
+    pretrained_means = [0.485, 0.456, 0.406]
+    pretrained_stds = [0.229, 0.224, 0.225]
 
-    train_transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(PRETRAINED_SIZE),
-        torchvision.transforms.RandomRotation(5),
-        torchvision.transforms.RandomHorizontalFlip(0.5),
-        torchvision.transforms.RandomCrop(PRETRAINED_SIZE, padding=10),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(PRETRAINED_MEANS, PRETRAINED_STDS)
-    ])
-
-    train = torchvision.datasets.CIFAR10(
-        root="./data",
-        train=True,
-        download=True,
-        transform=train_transform,
-    )
-    return train
-
-
-@pytest.fixture
-def validation():
     test_transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(PRETRAINED_SIZE),
+        torchvision.transforms.Resize(pretrained_size),
         torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(PRETRAINED_MEANS, PRETRAINED_STDS)
+        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
     ])
 
     test = torchvision.datasets.CIFAR10(
@@ -42,4 +21,5 @@ def validation():
         download=True,
         transform=test_transform,
     )
-    return test
+    data, _ = torch.utils.data.random_split(test, [100, len(test) - 100])
+    return data
--- tests/test_cnn_features.py
+++ tests/test_cnn_features.py
@@ -1,8 +1,7 @@
 from model.model import build_features
 
 
-def test_creates_features(train, validation):
-    print(len(train))
+def test_creates_features(data):
     model = build_features()
     model.initialize()
-    model.predict(validation)
+    assert model.predict_proba(data).shape == (len(data), 1000)
