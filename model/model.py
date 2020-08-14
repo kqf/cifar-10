baf2507fa73a2f96c1df268d8608e70903139f54
Sasha <q.dev.null@gmail.com>
2020-08-14 07:54:40 +0200

    Replace the tf modules with pytorch

--- model/model.py
+++ model/model.py
@@ -1,75 +1,88 @@
-import tensorflow as tf
-import tensornets
-import numpy as np
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.pipeline import make_pipeline
-from sklearn.svm import SVC
-from sklearn.decomposition import PCA
+import skorch
+import torch
+import torchvision
+from sklearn.metrics import accuracy_score
 
 
-class ReportShape(BaseEstimator, TransformerMixin):
-    def __init__(self, msg):
-        self.msg = msg
+class VisualModule(torch.nn.Module):
+    def __init__(self, backbone, classify=False, n_output):
+        super().__init__()
+        self.backbone = backbone
 
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        print("Reporting shape for {}: {}".format(self.msg, X.shape))
-        return X
+    def forward(self, x):
+        return self.backbone(x)
 
 
-class CNNFeatures(BaseEstimator, TransformerMixin):
-    def __init__(self, net=tensornets.ResNet50, stem=False, n_batches=10):
-        self.net = net
-        self.model = None
-        self.inputs = None
-        self.stem = stem
-        self.n_batches = n_batches
+class VisionClassifierNet(skorch.NeuralNet):
+    def predict(self, dataset):
+        probas = self.predict_proba(dataset)
+        return probas.argmax(-1)
 
-    def fit(self, X, y=None):
-        input_shape = list(X.shape)
-        input_shape[0] = None
-        self.inputs = tf.placeholder(tf.float32, shape=input_shape)
-        self.model = self.net(self.inputs, stem=self.stem)
-        return self
-
-    def transform(self, X):
-        size = X.shape[0]
-        _, n_features = self.model.get_shape().as_list()
-        preds = np.zeros((size, n_features))
-        with tf.Session() as sess:
-            for indeces in np.array_split(np.arange(size), self.n_batches):
-                imgs = self.model.preprocess(X[indeces].astype(np.float32))
-                sess.run(self.model.pretrained())
-                preds[indeces] = sess.run(self.model, {self.inputs: imgs})
-        return preds
+    def score(self, X, y):
+        preds = self.predict(X)
+        return accuracy_score(preds, y)
 
 
-def build_model(n_components=100):
-    model = make_pipeline(
-        CNNFeatures(),
-        ReportShape("CNN features"),
-        PCA(n_components=n_components, whiten=True),
-        SVC(gamma="scale"),
+def build_model(batch_norm=True, lr=1e-4):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    backbone = torchvision.models.resnet18(pretrained=True)
+    model = VisionClassifierNet(
+        module=Classifier,
+        module_backbone=backbone,
+        criterion=torch.nn.CrossEntropyLoss,
+        optimizer=torch.optim.Adam,
+        optimizer__lr=lr,
+        max_epochs=2,
+        batch_size=256,
+        iterator_train=DataIterator,
+        iterator_train__shuffle=True,
+        iterator_valid=DataIterator,
+        iterator_valid__shuffle=False,
+        device=device,
     )
     return model
 
 
-def build_shallow_model(n_components=100):
-    model = make_pipeline(
-        FunctionTransformer(
-            lambda x: x.reshape(x.shape[0], -1),
-            validate=False),
-        ReportShape("Pixel features"),
-        PCA(n_components=n_components, whiten=True),
-        ReportShape("PCA"),
-        SVC(gamma="scale"),
+def main():
+    # See https://pytorch.org/docs/stable/torchvision/models.html
+    pretrained_size = 224
+    pretrained_means = [0.485, 0.456, 0.406]
+    pretrained_stds = [0.229, 0.224, 0.225]
+
+    train_transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(pretrained_size),
+        torchvision.transforms.RandomRotation(5),
+        torchvision.transforms.RandomHorizontalFlip(0.5),
+        torchvision.transforms.RandomCrop(pretrained_size, padding=10),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
+    ])
+
+    test_transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(pretrained_size),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
+    ])
+
+    train = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=True,
+        download=True,
+        transform=train_transform,
     )
-    # This yields following result
-    # Train score 0.8108
-    # Test  score 0.4665
-    # It's clear overfit, one can regularize this model
-    # by decreasing C, or by reducing n_components
-    return model
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = build_model(device).fit(train)
+
+    test = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=False,
+        download=True,
+        transform=test_transform,
+    )
+    print(model.predict(test))
+
+
+if __name__ == "__main__":
+    main()
