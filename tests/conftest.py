fcc96a4b7f87eeabf351ed196024392dbc96555f
Sasha <q.dev.null@gmail.com>
2020-08-14 16:24:18 +0200

    Fix the tests for CNN features

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,45 @@
-import os
 import pytest
+import torchvision
+
+PRETRAINED_SIZE = 224
+PRETRAINED_MEANS = [0.485, 0.456, 0.406]
+PRETRAINED_STDS = [0.229, 0.224, 0.225]
 
 
-@pytest.fixture()
-def datapath():
-    datapath = "data/cifar-10-batches-py"
-    assert os.path.exists(datapath)
-    return datapath
+@pytest.fixture
+def train():
+    # See https://pytorch.org/docs/stable/torchvision/models.html
+
+    train_transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(PRETRAINED_SIZE),
+        torchvision.transforms.RandomRotation(5),
+        torchvision.transforms.RandomHorizontalFlip(0.5),
+        torchvision.transforms.RandomCrop(PRETRAINED_SIZE, padding=10),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(PRETRAINED_MEANS, PRETRAINED_STDS)
+    ])
+
+    train = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=True,
+        download=True,
+        transform=train_transform,
+    )
+    return train
+
+
+@pytest.fixture
+def validation():
+    test_transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(PRETRAINED_SIZE),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(PRETRAINED_MEANS, PRETRAINED_STDS)
+    ])
+
+    test = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=False,
+        download=True,
+        transform=test_transform,
+    )
+    return test
--- tests/test_cnn_features.py
+++ tests/test_cnn_features.py
@@ -1,15 +1,8 @@
-import pytest
-from model.model import CNNFeatures
-from model.data import train_test_sample
+from model.model import build_features
 
 
-@pytest.mark.skip()
-@pytest.mark.filterwarnings("ignore:RuntimeWarning")
-def test_creates_features(datapath):
-    X_tr, X_te, y_tr, y_te = train_test_sample(datapath, size=0.1)
-
-    print("Total training size", X_tr.shape[0], "images")
-    print("Total training size", X_tr.nbytes / 1024 / 1024, "mb")
-
-    features = CNNFeatures().fit_transform(X_tr)
-    assert features.shape == (X_tr.shape[0], 1000)
+def test_creates_features(train, validation):
+    print(len(train))
+    model = build_features()
+    model.initialize()
+    model.predict(validation)
