e8ef1193da9118d4f65c69a5aa5eb97e0c2d93f8
Sasha <q.dev.null@gmail.com>
2020-08-16 09:03:23 +0200

    Fix the broken CI

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,14 +1,13 @@
 import pytest
-from model.model import build_model, build_shallow_model
-from model.data import train_test_sample
+
+from model.model import build_model
+# from model.model import build_shallow_model
 
 
-@pytest.mark.skip()
 @pytest.mark.parametrize("build", [
     build_model,
-    build_shallow_model
+    # build_shallow_model
 ])
-def test_handles_model(datapath, build):
-    X_tr, X_te, y_tr, y_te = train_test_sample(datapath, size=0.001)
-    model = build(n_components=2).fit(X_tr, y_tr)
-    assert model.score(X_tr, y_tr) > 0.1
+def test_handles_model(build, data):
+    model = build().fit(data)
+    assert model.score(data) > 0.1
