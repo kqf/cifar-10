80646ea1b9e2400db440385b4c8b2d2894b54f26
Sasha <q.dev.null@gmail.com>
2020-08-22 08:42:32 +0200

    Add the implicit tests for train_test_split functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,32 +1,17 @@
 import pytest
 import torch
-import torchvision
 
-from model.cnnfeatures import tolabels
+from model.cnnfeatures import tolabels, train_test_set
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def data(size=100):
-    pretrained_size = 224
-    pretrained_means = [0.485, 0.456, 0.406]
-    pretrained_stds = [0.229, 0.224, 0.225]
-
-    test_transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(pretrained_size),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
-    ])
-
-    test = torchvision.datasets.CIFAR10(
-        root="./data",
-        train=False,
-        download=True,
-        transform=test_transform,
-    )
-    data, _ = torch.utils.data.random_split(test, [size, len(test) - size])
+    # Test the train test set method here
+    X_tr, *_ = train_test_set()
+    data, _ = torch.utils.data.random_split(X_tr, [size, len(X_tr) - size])
     return data
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def labels(data):
     return tolabels(data)
