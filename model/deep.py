40a1a404602f74c6f6ca3bca4ce8abc42d79d571
Sasha <q.dev.null@gmail.com>
2020-08-24 14:30:12 +0200

    Clean the dataset preparation

--- model/deep.py
+++ model/deep.py
@@ -1,6 +1,48 @@
 import skorch
 import torch
 import torchvision
+import numpy as np
+
+
+def tolabels(data):
+    return np.array([l for _, l in data])
+
+
+def train_test_set():
+    # See https://pytorch.org/docs/stable/torchvision/models.html
+    pretrained_size = 224
+    pretrained_means = [0.485, 0.456, 0.406]
+    pretrained_stds = [0.229, 0.224, 0.225]
+
+    train_transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(pretrained_size),
+        torchvision.transforms.RandomRotation(5),
+        torchvision.transforms.RandomHorizontalFlip(0.5),
+        torchvision.transforms.RandomCrop(pretrained_size, padding=10),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
+    ])
+
+    test_transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(pretrained_size),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
+    ])
+
+    train = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=True,
+        download=True,
+        transform=train_transform,
+    )
+
+    test = torchvision.datasets.CIFAR10(
+        root="./data",
+        train=False,
+        download=True,
+        transform=test_transform,
+    )
+    return train, test, tolabels(train), tolabels(test)
 
 
 class ClassifierModule(torch.nn.Module):
@@ -31,46 +73,10 @@ def build_model(lr=1e-4, max_epochs=2):
 
 
 def main():
-    # See https://pytorch.org/docs/stable/torchvision/models.html
-    pretrained_size = 224
-    pretrained_means = [0.485, 0.456, 0.406]
-    pretrained_stds = [0.229, 0.224, 0.225]
-
-    print(f'Calculated means: {pretrained_means}')
-    print(f'Calculated stds: {pretrained_stds}')
-
-    train_transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(pretrained_size),
-        torchvision.transforms.RandomRotation(5),
-        torchvision.transforms.RandomHorizontalFlip(0.5),
-        torchvision.transforms.RandomCrop(pretrained_size, padding=10),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
-    ])
-
-    test_transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(pretrained_size),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(pretrained_means, pretrained_stds)
-    ])
-
-    train = torchvision.datasets.CIFAR10(
-        root="./data",
-        train=True,
-        download=True,
-        transform=train_transform,
-    )
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = build_model(device).fit(train)
-
-    test = torchvision.datasets.CIFAR10(
-        root="./data",
-        train=False,
-        download=True,
-        transform=test_transform,
-    )
-    print(model.predict(test))
+    X_tr, X_te, y_tr, y_te = train_test_set()
+    model = build_model().fit(X_tr, y_tr)
+    print(model.score(X_tr, y_tr))
+    print(model.score(X_te, y_te))
 
 
 if __name__ == "__main__":
