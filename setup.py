8b9c46765c9d7ba7ae5f9143df78ddbec09cf80b
Sasha <q.dev.null@gmail.com>
2020-08-26 07:06:54 +0200

    Fix the deep endpoint

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
             "draw-cnn-features=model.visualize:draw_cnn_features",
             "train-shallow=model.shallow:main",
             "train-cnn-features=model.cnnfeatures:main",
-            "train-deep-model=model.cnnfeatures:main",
+            "train-deep-model=model.deep:main",
         ],
     }
 )
